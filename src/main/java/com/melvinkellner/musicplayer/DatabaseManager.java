6c726898b913f1d26fe2e79f8f33ee53c9af8375
melvin-kellner <melvin@melvin-kellner.com>
2015-04-13 10:35:26 +0200

    bugfixes voor streaming

--- src/main/java/com/melvinkellner/musicplayer/DatabaseManager.java
+++ src/main/java/com/melvinkellner/musicplayer/DatabaseManager.java
@@ -459,6 +459,7 @@ public class DatabaseManager
     }
     currentRequests = songs;
     requestJSON = new Gson().toJson(DatabaseManager.instance.currentRequests);
+    StreamManager.instance.revalidateCache();
   }
 
   public void setVisiblePlayList() throws SQLException
